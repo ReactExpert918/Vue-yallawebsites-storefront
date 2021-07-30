fa345e9a618a77280633180f76abdbf10bd0a988
ReactExpert918 <potterharrry0918@gmail.com>
2021-07-30 12:53:18 +0300

    update invoice pagination

--- src/router/views/sales/invoices/invoices.vue
+++ src/router/views/sales/invoices/invoices.vue
@@ -20,6 +20,7 @@ export default {
       backendURL: process.env.VUE_APP_BACKEND_URL,
       selectedAll: false,
       invoicesData: [],
+      invoicesDataLength: [],
       title: "Invoices",
       items: [
         {
@@ -85,7 +86,7 @@ export default {
         * Total no. of records
         */
       rows() {
-          return this.invoicesData.length;
+          return this.invoicesDataLength;
       },
   },
   watch: {
@@ -106,6 +107,7 @@ export default {
       .get(`${this.backendURL}/api/v1/orders/invoices?per_page=${this.perPage}&page=${this.currentPage}` , authHeader())
       .then(response => {
          this.invoicesData = response.data.data;
+         this.invoicesDataLength = response.data.pagination.total;
          for (var i = 0; i < this.invoicesData.length; i++){
             var inv = this.invoicesData[i];
             if (inv.order == null){
@@ -126,6 +128,21 @@ export default {
           // Trigger pagination to update the number of buttons/pages due to filtering
           this.totalRows = filteredItems.length;
           this.currentPage = 1;
+      },
+      handlePageChange(value) {
+        this.currentPage = value;
+        axios
+        .get(`${this.backendURL}/api/v1/orders/invoices?per_page=${this.perPage}&page=${this.currentPage}` , authHeader())
+        .then(response => (this.invoicesData = response.data.data,
+                           this.invoicesDataLength = response.data.pagination.total));
+      },
+      handlePerPageChange(value) {
+        this.perPage = value;
+        this.currentPage = 1;
+        axios
+        .get(`${this.backendURL}/api/v1/orders/invoices?per_page=${this.perPage}&page=${this.currentPage}` , authHeader())
+        .then(response => (this.invoicesData = response.data.data,
+                           this.invoicesDataLength = response.data.pagination.total));
       }
   },
 };
@@ -158,7 +175,13 @@ export default {
                   <div id="tickets-table_length" class="dataTables_length">
                       <label class="d-inline-flex align-items-center">
                           Show&nbsp;
-                          <b-form-select v-model="perPage" size="sm" :options="pageOptions"></b-form-select>&nbsp;entries
+                        <b-form-select 
+                          v-model="perPage" 
+                          size="sm"  
+                          :options="pageOptions"
+                          @change = "handlePerPageChange"
+                        >
+                        </b-form-select>&nbsp;entries
                       </label>
                   </div>
                 </div>
@@ -177,7 +200,7 @@ export default {
                       :fields="fields" 
                       responsive="sm" 
                       :per-page="perPage" 
-                      :current-page="currentPage" 
+                      :current-page="1" 
                       :sort-by.sync="sortBy" 
                       :sort-desc.sync="sortDesc" 
                       :filter="filter" 
@@ -225,7 +248,13 @@ export default {
                         <div class="dataTables_paginate paging_simple_numbers float-right">
                             <ul class="pagination pagination-rounded mb-0">
                                 <!-- pagination -->
-                                <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"></b-pagination>
+                              <b-pagination 
+                                v-model="currentPage" 
+                                :total-rows="rows" 
+                                :per-page="perPage"
+                                @change = "handlePageChange"
+                              >
+                              </b-pagination>
                             </ul>
                         </div>
                     </div>
